1c59b994eb4c3ba8fa9ad5fb13b748aaa6085c82
Donnie D'Amato <fauxserious@gmail.com>
2023-12-28 12:51:16 -0500

    Basic formatting

--- src/content/config.js
+++ src/content/config.js
@@ -2,13 +2,8 @@ import { z, defineCollection } from 'astro:content';
 
 export const collections = {
   'posts': defineCollection({
-    type: 'content', // v2.5.0 and later
-    schema: z.object({
-      title: z.string(),
-      desc: z.string(),
-      date: z.date(),
-      draft: z.boolean().optional(),
-    }),
+    type: 'pages', // v2.5.0 and later
+    schema: z.object({}),
   }),
 };
 
